54f97a13e2d14ae5de9c8065995ca85ee600776a
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-07 23:07:14 +0530

    read content of file.

--- ReadLines.java
+++ ReadLines.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ReadLines {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+public class ReadLines {
+    public static void main(String[] args) {
+        Path filepath = Paths.get("src/main/java/file.txt");
+        try(Stream<String> lines = Files.lines(filepath)){
+            lines.forEach(System.out::println);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
