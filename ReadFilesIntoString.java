cc767a9393bc7894951c64800c11dddbad10c42d
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-07 23:06:46 +0530

    Read file content as string.

--- ReadFilesIntoString.java
+++ ReadFilesIntoString.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class ReadFilesIntoString {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class ReadFilesIntoString {
+    public static void main(String[] args) {
+        Path file = Paths.get("src/main/java/renamedFile.txt");
+        try {
+            String content = Files.readString(file);
+            System.out.println(content);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
