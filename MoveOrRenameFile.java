738d80d10ce922bd8473acf0063488510dd15339
Vineet Thakran <Vineet.Thakran@godigit.com>
2024-08-07 23:06:18 +0530

    Rename file program.

--- MoveOrRenameFile.java
+++ MoveOrRenameFile.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class MoveOrRenameFile {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
+public class MoveOrRenameFile {
+    public static void main(String[] args) {
+        Path file = Paths.get("src/main/java/file.txt");
+        Path renamedFile = Paths.get("src/main/java/renamedFile.txt");
+        try{
+            Files.move(file, renamedFile, StandardCopyOption.REPLACE_EXISTING);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 }
